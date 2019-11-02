04c8352355d696528bf59c94377534d86af529fe
dthtien <tiendt2311@gmail.com>
2019-11-02 15:57:45 +0700

    fix(config): move api url to .env file

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const httpClient = () => {
   const instance = axios.create({
-    baseURL: "http://localhost:6060/api/v1"
+    baseURL: process.env.REACT_APP_AP_URL
   });
 
   return instance;
