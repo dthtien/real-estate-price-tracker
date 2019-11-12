435ecae4ca4990fdd851830ece8f5aad0804493b
Anthony <tien.dau@dounets.com>
2019-11-12 10:04:51 +0700

    feat(deploy): force ssl on heroku server

--- gulpfile.js
+++ gulpfile.js
@@ -5,70 +5,19 @@ gulp.task("licenses", async function() {
   // this is to add Creative Tim licenses in the production mode for the minified js
   gulp
     .src("build/static/js/*chunk.js", { base: "./" })
-    .pipe(
-      gap.prependText(`/*!
-
-=========================================================
-* Material Dashboard React - v1.8.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/now-ui-kit-pro-react
-* Copyright 2019 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/`)
-    )
+    .pipe(gap.prependText("dthtien - https://github.com/dthtien"))
     .pipe(gulp.dest("./", { overwrite: true }));
 
   // this is to add Creative Tim licenses in the production mode for the minified html
   gulp
     .src("build/index.html", { base: "./" })
-    .pipe(
-      gap.prependText(`<!--
-
-=========================================================
-* Material Dashboard React - v1.8.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/now-ui-kit-pro-react
-* Copyright 2019 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
--->`)
-    )
+    .pipe(gap.prependText("dthtien - https://github.com/dthtien"))
     .pipe(gulp.dest("./", { overwrite: true }));
 
   // this is to add Creative Tim licenses in the production mode for the minified css
   gulp
     .src("build/static/css/*chunk.css", { base: "./" })
-    .pipe(
-      gap.prependText(`/*!
-
-=========================================================
-* Material Dashboard React - v1.8.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/now-ui-kit-pro-react
-* Copyright 2019 Creative Tim (http://www.creative-tim.com)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/`)
-    )
+    .pipe(gap.prependText("dthtien - https://github.com/dthtien"))
     .pipe(gulp.dest("./", { overwrite: true }));
   return;
 });
