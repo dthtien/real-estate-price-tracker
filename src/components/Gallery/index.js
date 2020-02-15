8c7c598394dd3623c0d7a77291c0c661ce42b5f9
Dau The Tien <tiendt2311@gmail.com>
2020-02-15 17:51:12 +0700

    feat(land show): make gallery look better

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import GridContainer from "components/Grid/GridContainer";
 import GridItem from "components/Grid/GridItem";
 import PropTypes from "prop-types";
-import Zoom from "react-medium-image-zoom";
-import "react-medium-image-zoom/dist/styles.css";
+import Carousel, { Modal, ModalGateway } from "react-images";
 import { useTranslation } from "react-i18next";
 
 const useStyles = makeStyles(theme => ({
@@ -21,29 +20,46 @@ const useStyles = makeStyles(theme => ({
   },
   title: {
     paddingBottom: 15
+  },
+  image: {
+    width: 200,
+    height: 200,
+    cursor: "pointer"
   }
 }));
 
 const ImageGridList = ({ images }) => {
   const classes = useStyles();
   const { t } = useTranslation();
-
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const handleImageClicked = index => {
+    setIsOpen(true);
+    setCurrentIndex(index);
+  };
+  const renderingImages = images.map(image => ({ src: image.origin }));
+  const toggleModal = () => setIsOpen(!isOpen);
   return (
     <div className={classes.root}>
       <h4 className={classes.title}>{t("Preview Images")}</h4>
       <GridContainer className={classes.gridList}>
-        {images.map(image => (
+        {images.map((image, index) => (
           <GridItem key={image.thumb} md={2} sm={6} xs={12}>
-            <Zoom>
-              <img
-                src={image.origin}
-                alt={image.thumb}
-                width={200}
-                height={200}
-              />
-            </Zoom>
+            <img
+              src={image.thumb}
+              alt={image.thumb}
+              className={classes.image}
+              onClick={() => handleImageClicked(index)}
+            />
           </GridItem>
         ))}
+        <ModalGateway>
+          {isOpen ? (
+            <Modal onClose={toggleModal}>
+              <Carousel currentIndex={currentIndex} views={renderingImages} />
+            </Modal>
+          ) : null}
+        </ModalGateway>
       </GridContainer>
     </div>
   );
